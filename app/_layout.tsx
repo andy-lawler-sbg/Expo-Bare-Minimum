1a454115a752cec051af638bdce172a31159a7ec
Andy Lawler <andrew.lawler@skybettingandgaming.com>
2023-01-27 09:38:35 +0000

    Changed header styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,14 @@ import { Stack } from "expo-router";
 const Layout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerLargeTitle: true,
+          headerTransparent: true,
+        }}
+      />
     </Stack>
   );
 };
