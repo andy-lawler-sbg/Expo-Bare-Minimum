d9a15cafff8e77244b1a618982ba365dc9698cd7
Andy Lawler <andrew.lawler@skybettingandgaming.com>
2023-01-31 15:55:17 +0000

    Added HeaderStyle for index

--- app/index.tsx
+++ app/index.tsx
@@ -2,11 +2,16 @@ import HomeScreen from "../Screens/HomeScreen/HomeScreen";
 import { Stack } from "expo-router";
 import { useState } from "react";
 
+type HeaderStyle = {
+  title: string;
+  color: string;
+};
+
 const Index = () => {
-  const [headerStyle, setHeaderStyle] = useState<{
-    title: string;
-    color: string;
-  }>({ title: "Login", color: "#63d4c5" });
+  const [headerStyle, setHeaderStyle] = useState<HeaderStyle>({
+    title: "Login",
+    color: "#63d4c5",
+  });
 
   const didLogin = () => {
     setHeaderStyle({ title: "Welcome", color: "#64d975" });
