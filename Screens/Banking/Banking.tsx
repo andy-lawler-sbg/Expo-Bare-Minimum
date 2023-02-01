f39b02bf98a8c839fc5cd1b5e709ccd5d7183add
Andy Lawler <andrew.lawler@skybettingandgaming.com>
2023-02-01 16:29:40 +0000

    added type for bank mock

--- Screens/Banking/Banking.tsx
+++ Screens/Banking/Banking.tsx
@@ -2,31 +2,31 @@ import { View, FlatList } from "react-native";
 import { H2, P } from "@expo/html-elements";
 
 import { styles } from "../../utils";
+import { AccountRow } from "./Banking.types";
 
 // This example shows Expo-HTML-Elements working in a bare workflow app
 
-const Banking = () => {
-  const data = [
-    { id: "1", price: "10" },
-    { id: "2", price: "20" },
-  ];
-  return (
-    <View style={styles.container}>
-      <H2>Transactions</H2>
-      <FlatList
-        style={{ margin: 20 }}
-        data={data}
-        renderItem={({ item }) => {
-          return (
-            <P>
-              {item.id}: ${item.price}
-            </P>
-          );
-        }}
-        keyExtractor={(item) => item.id}
-      />
-    </View>
-  );
-};
+const data: AccountRow[] = [
+  { id: "1", price: 10 },
+  { id: "2", price: 20 },
+];
+
+const Banking = () => (
+  <View style={styles.container}>
+    <H2>Transactions</H2>
+    <FlatList
+      style={{ margin: 20 }}
+      data={data}
+      renderItem={({ item }) => {
+        return (
+          <P>
+            {item.id}: ${item.price}
+          </P>
+        );
+      }}
+      keyExtractor={(item) => item.id}
+    />
+  </View>
+);
 
 export default Banking;
---
+++ Screens/Banking/Banking.types.ts
@@ -0,0 +1,4 @@
+export type AccountRow = {
+  id: string;
+  price: number;
+};
