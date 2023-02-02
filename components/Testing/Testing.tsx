a951c9527aad13b8923f98f27167a743fac24fbb
Andy Lawler <andrew.lawler@skybettingandgaming.com>
2023-02-02 14:16:17 +0000

    Committed final bit

--- components/Testing/Testing.tsx
+++ components/Testing/Testing.tsx
@@ -5,10 +5,7 @@ import styles from "./Testing.styles";
 export const Testing = () => {
   return (
     <View testID="TestingView" style={styles.container}>
-      <Image
-        source={require("../../assets/Apple.png")}
-        style={{ width: 30, height: 30, tintColor: "white" }}
-      />
+      <Image source={require("../../assets/Apple.png")} style={styles.image} />
       <H4 style={styles.text}>Hello World</H4>
     </View>
   );
